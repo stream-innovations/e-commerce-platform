afd4d7329dde88a3efebdf3b1957102be152dfdd
Stream Protocol Ltd <streamprotocol@protonmail.com>
2023-06-22 09:11:27 +0000

    commit

--- apps/backend/medusa-config.js
+++ apps/backend/medusa-config.js
@@ -29,9 +29,9 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,https://streampay.store";
 
 const DATABASE_URL =
-    process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+    process.env.DATABASE_URL || "postgres://hunfzuexskynga:308bd31faf709282666344e67655c2dd39a98e44f08ce51986d94bcacd87614f@ec2-52-19-55-12.eu-west-1.compute.amazonaws.com:5432/d1dq3su9eah4o2";
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL || "redis://default:JXxWvP1twsaS3LleLUubr48my3Ke2hBE7IX5cdvULQeTNsbrq8uCWjxQlzuYSda7@6qcck4.stackhero-network.com:6379";
 
 // Stripe keys
 const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "pk_live_51N5Q0DKjdN5iZkcXhxtDR5qcjJw6sGAWmQGst8PP4OwE9WeV5kvYu5b7dJO2Gzbl9DLee9ZQAjwX0hoGvhrbJeKk004uB84Buc";
